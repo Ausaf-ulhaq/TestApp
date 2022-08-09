497b7648bf95ade2d55765dbc4e5ae9cdff4c4e0
Ausaf <ausafulhaqshaikh@gmail.com>
2022-08-09 17:25:01 +0530

    small changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,18 @@ import React, {Component} from 'react';
 import './Header.css'
 
 class Header extends Component{
-    
-    
+    constructor(){
+        super()
+
+        this.state={
+            title:"React Application",
+            keyword:"User Text Here"
+        }
+    }
+    handelChange = (event) => {
+        console.log(event.target.value)
+        this.setState({keyword:event.target.value?event.target.value:'User Text Here'})
+    }
     render(){
         console.log("Inside render")
         const myStyle={
@@ -11,24 +21,23 @@ class Header extends Component{
                 backgroundColor:'teal'
             }
         }
+        
         return(
             <>
             <header style={myStyle.header}>
              <center>
-             <div className='logo' id='logo-1'>React App</div>   
-                <input />
-                <div style={{color:"white", fontSize:'20px'}}>User Text Here</div>
+             <div className='logo' id='logo-1'>{this.state.title}</div>   
+
+                <input className="input-txt" onChange={this.handelChange}/>
+
+                <div style={{color:"white", fontSize:'20px'}}>{this.state.keyword}</div>
              </center>
             </header>
             <hr/>
             </>
         )
     }
-    constructor(){
-        super()
-
-        console.log("Inside constructor")
-    }
+    
 }
 
 export default Header;
